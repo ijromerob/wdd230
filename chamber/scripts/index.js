4970d486f670758f55ee96aac53ca7917e4bd18f
ijromerob <ijromerob@gmail.com>
2024-06-22 18:53:55 -0600

    Chamber of commerce - updates

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,3 +1,17 @@
+
+//get date for the website
 document.getElementById('year').textContent = new Date().getFullYear();
 
-document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
+document.getElementById('lastmodified').textContent = `Last modified: ${document.lastModified}`;
+
+
+//hamburguer button
+
+const hbutton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hbutton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hbutton.classList.toggle('open');
+});
+
