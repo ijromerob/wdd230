ee82bf0bc97bf0675c9f1263dc837c733fcb656b
ijromerob <ijromerob@gmail.com>
2024-06-25 23:34:37 -0600

    Local Storage Learning Activity

---
+++ week03/scripts/bom.js
@@ -0,0 +1,51 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+
+button.addEventListener('click', () => {
+    if (input.value !== "") {
+        displayList(input.value)
+        chaptersArray.push(input.value)
+        setChapterList();
+        input.value = "";
+        input.focus();
+    }
+})
+
+
+
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter)
+});
+
+function displayList(item) {
+    const txt = document.createElement('li');
+    const delb = document.createElement('button');
+    txt.textContent = item;
+    delb.textContent = '❌';
+    txt.appendChild(delb);
+    list.appendChild(txt);
+    delb.addEventListener('click', () => {
+        list.removeChild(txt);
+        deleteChapter(txt.textContent);
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem('FavoriteBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('FavoriteBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+    setChapterList();
+
+}
