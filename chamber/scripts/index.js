1edc3436db78afabd6cebd6e6fe45c17d8bb91ae
ijromerob <ijromerob@gmail.com>
2024-06-28 14:32:00 -0600

    Adding effect while hovering the images in the large view, Added the message when a user enters the page

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -15,3 +15,37 @@ hbutton.addEventListener('click', () => {
     hbutton.classList.toggle('open');
 });
 
+
+// Welcome message in the side-bar
+
+const txt = document.querySelector('#welcome');
+
+let date = getDate() || 0;
+
+if (date !== 0) {
+    let oldDate = new Date(date)
+    let dif = (Date.now() - oldDate.getTime()) / (1000 * 60 * 60 * 24) + 1; // difference in days
+    if (dif < 1) {
+        txt.textContent = 'Back so soon! Awesome!';
+        date = Date.now();
+        setDate(date);
+    }
+    else {
+        txt.textContent = `You last visited ${Math.floor(dif)} day(s) ago.`;
+        date = Date.now();
+        setDate(date);
+    }
+}
+else {
+    txt.textContent = 'Let us know if you have any questions!';
+    date = Date.now();
+    setDate(date);
+}
+
+function getDate() {
+    return Number(JSON.parse(localStorage.getItem('visit-date')));
+}
+
+function setDate(item) {
+    localStorage.setItem('visit-date', JSON.stringify(item));
+}
