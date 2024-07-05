c9a03a87fbb36c11fbe15468d0a56c0308421660
ijromerob <ijromerob@gmail.com>
2024-07-05 16:08:24 -0600

    Adjusted CSS and a few changes in HTML to help enhance the form

---
+++ chamber/scripts/join.js
@@ -0,0 +1,38 @@
+//get date for the website
+document.getElementById('year').textContent = new Date().getFullYear();
+
+document.getElementById('lastmodified').textContent = `Last modified: ${document.lastModified}`;
+
+
+//hamburguer button
+
+const hbutton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hbutton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hbutton.classList.toggle('open');
+});
+
+// time stamp
+
+function convertToLocal(date) {
+    let year = date.getFullYear();
+    let month = (date.getMonth() + 1).toString().padStart(2, "0");
+    let day = date.getDate().toString().padStart(2, "0");
+    let hours = date.getHours().toString().padStart(2, "0");
+    let minutes = date.getMinutes().toString().padStart(2, "0");
+
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
+}
+
+
+const timestamp = document.querySelector('#timestamp');
+const submit = document.querySelector('#reg');
+
+
+submit.addEventListener('click', () => {
+    const datenow = new Date();
+    timestamp.value = convertToLocal(datenow);
+})
+
