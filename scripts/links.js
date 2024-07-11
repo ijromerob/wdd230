01807e415f3d011416192a9ca96f279cafe05188
ijromerob <ijromerob@gmail.com>
2024-07-10 20:58:41 -0600

    team activity links updated

--- scripts/links.js
+++ scripts/links.js
@@ -1,12 +1,30 @@
 const baseURL = 'https://ijromerob.github.io/wdd230/';
 const linksURL = 'https://ijromerob.github.io/wdd230/data/links.json'
+const linklist = document.querySelector('#link-list')
 
 async function getLinksData() {
     const response = await fetch(linksURL);
     if (response.ok) {
         const data = await response.json();
-        console.log(data);
+        displayLinks(data);
     }
 }
 
 getLinksData()
+
+function displayLinks({ weeks }) {
+    weeks.forEach(({ week, links }) => {
+        const listElement = document.createElement('li');
+        listElement.innerText = `${week}: `;
+        links.forEach(({ url, title }) => {
+            const linkElement = document.createElement('a');
+            if (url.substring(0, 4) !== 'http') {
+                url = baseURL + url;
+            }
+            linkElement.setAttribute('href', url);
+            linkElement.innerText = `${title} | `;
+            listElement.appendChild(linkElement);
+        })
+        linklist.appendChild(listElement);
+    })
+}
