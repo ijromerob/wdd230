e5c1611826942ce0c86b46af1589f760d0b2eafb
ijromerob <ijromerob@gmail.com>
2024-07-16 18:14:43 -0600

    page enhancements - added the member's ads and the banner appearing every Mon, Tue, and Wed.

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -30,6 +30,7 @@ function setDate(item) {
 const weatherCard = document.querySelector('#weather-container');
 const weatherAPIURL = "https://api.openweathermap.org/data/2.5/weather?lat=51.045763653662505&lon=-114.07200932666383&appid=2838e61f6d31a85f3212a3c686b49e64&units=metric";
 const forecastAPIURL = "https://api.openweathermap.org/data/2.5/forecast?q=calgary&appid=2838e61f6d31a85f3212a3c686b49e64&units=metric&cnt=24";
+const membersURL = "https://ijromerob.github.io/wdd230/chamber/data/members.json";
 const forecastCard = document.querySelector('#forecast-container');
 
 // Current Weather
@@ -38,12 +39,18 @@ async function apiFetch(url, current) { // false for weather forecaset, true for
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            if (current) {
-                displayCurrentWeather(data)
+            switch (current) {
+                case 1:
+                    displayCurrentWeather(data);
+                    break;
+                case 2:
+                    displayForecastWeather(data);
+                    break;
+                case 3:
+                    displaySpotlightsAds(data);
+                    break;
             }
-            else {
-                displayForecastWeather(data)
-            }
+
         }
         else {
             throw Error(await response.text());
@@ -53,7 +60,7 @@ async function apiFetch(url, current) { // false for weather forecaset, true for
     }
 }
 
-apiFetch(weatherAPIURL, true); // current weather
+apiFetch(weatherAPIURL, 1); // current weather
 
 function displayCurrentWeather({ main: { temp }, weather: [{ main, description, icon }] }) {
 
@@ -72,7 +79,7 @@ function displayCurrentWeather({ main: { temp }, weather: [{ main, description, 
 
 }
 
-apiFetch(forecastAPIURL, false) // weather forecast
+apiFetch(forecastAPIURL, 2) // weather forecast
 let cardImages = [1, 2, 3];
 
 function displayForecastWeather({ list }) {
@@ -98,15 +105,18 @@ function displayForecastWeather({ list }) {
         const date = new Date(dt * 1000);
         const weekday = daysOfWeek[date.getDay()]; // Sun - Sat
         const monthday = date.getDate(); // Day of the month that it is being displayed (1-31)
-        const hour = date.getHours(); // hour of the day that it will be displayed
-        const minute = date.getMinutes(); // minutes of the day that is being displayed
+        const [hourTwelve, pmAM] = hours(date.getHours()); // hour of the day that it will be displayed
+        const minute = date.getMinutes().toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+        }); // minutes of the day that is being displayed
 
         miniCard.setAttribute('class', 'miniWeatherCard');
         heading.textContent = `${weekday} ${monthday}`;
         miniIcon.setAttribute('src', `https://openweathermap.org/img/w/${icon}.png`);
         miniIcon.setAttribute('alt', description);
         temperature.innerHTML = `${temp.toFixed(0)}&deg;C`;
-        time.innerText = `${hour}:${minute}`;
+        time.innerText = `${hourTwelve}:${minute} ${pmAM}`;
 
 
         miniCard.appendChild(heading);
@@ -165,5 +175,80 @@ function showCard(indexesToDisplay) {
         bleft.style.display = "block";
         bright.style.display = "block";
     }
+}
+
+function hours(hrs) {
+    let fixedHrs;
+    let pmAm;
+    let hrsPMAM = [];
+    if (hrs > 12) {
+        fixedHrs = hrs - 12;
+        pmAm = "PM"
+    }
+    else if (hrs === 12) {
+        fixedHrs = hrs;
+        pmAm = "PM";
+    }
+    else if (hrs === 0) {
+        fixedHrs = 12;
+        pmAm = "AM";
+    }
+    else {
+        fixedHrs = hrs;
+        pmAm = "AM";
+    }
+    hrsPMAM.push(fixedHrs, pmAm);
+    return hrsPMAM;
+}
+
+const companyAd = document.querySelectorAll('.company-ad');
+function displaySpotlightsAds({ companies }) {
+    const premiumCompanies = companies.filter(({ membershipLevel }) =>
+        membershipLevel === "Gold Membership" || membershipLevel === "Silver Membership");
+
+    const displayListCompanies = [];
+    const copyPremiumCompanies = [...premiumCompanies];
+
+    for (i = 0; i < 3; i++) {
+        const randomIndex = Math.floor(Math.random() * copyPremiumCompanies.length);
+        displayListCompanies.push(copyPremiumCompanies[randomIndex]);
+        copyPremiumCompanies.splice(randomIndex, 1);
+    }
+
+    for (i = 0; i < 3; i++) {
+        const name = document.createElement('h2');
+        const address = document.createElement('p');
+        const phone = document.createElement('p');
+        const website = document.createElement('a');
+        const membership = document.createElement('p');
+
+        name.textContent = displayListCompanies[i].name;
+        address.textContent = displayListCompanies[i].address;
+        phone.textContent = displayListCompanies[i].phone;
+        website.setAttribute('href', displayListCompanies[i].url);
+        website.textContent = "Company Website";
+        membership.textContent = displayListCompanies[i].membershipLevel;
+
+        companyAd[i].appendChild(name);
+        companyAd[i].appendChild(address);
+        companyAd[i].appendChild(phone);
+        companyAd[i].appendChild(website);
+        companyAd[i].appendChild(membership);
+
+    }
 
 }
+
+apiFetch(membersURL, 3);
+
+const datetoday = new Date();
+
+if (datetoday.getDay() > 0 && datetoday.getDay() < 4) {
+    document.querySelector('#event-banner').showModal();
+}
+
+const exitBanner = document.querySelector('#banner-exit');
+
+exitBanner.addEventListener('click', () => {
+    document.querySelector('#event-banner').close();
+});
